4b41a12c0a5ee3644b0310ce2c9e444a55c06017
julianbrandt <julianbrandt@godmail.dk>
2018-11-29 12:17:57 +0100

    lmao

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,10 +8,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //lmao
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello, World!");
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
