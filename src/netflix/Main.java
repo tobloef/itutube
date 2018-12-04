d11c21d12c3a469a98fbc0c26444ce909ff1378d
julianbrandt <julianbrandt@godmail.dk>
2018-12-04 20:17:55 +0100

    VERY functional database

--- src/netflix/Main.java
+++ src/netflix/Main.java
@@ -3,9 +3,14 @@ package netflix;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import netflix.models.media.Media;
 import netflix.views.pages.ContentPage;
 import netflix.views.creators.ViewCreator;
+
+import javax.xml.crypto.Data;
+import java.util.Arrays;
 
 public class Main extends Application {
 
@@ -13,11 +18,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+
         this.stage = stage;
         stage.setTitle("ITU-tube");
-        ContentPage contentPage = new ContentPage(this::setPage);
-        stage.setScene(new Scene(contentPage, 600, 400));
+        StackPane stackpane = new StackPane();
+        //ContentPage contentPage = new ContentPage(this::setPage);
+        stage.setScene(new Scene(stackpane, 600, 400));
         stage.show();
+
     }
 
     private void setPage(ViewCreator creator) {
@@ -26,7 +34,11 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        Database.load();
+        for(Media m : Database.getMediaList()) {
+            System.out.println(m.getId() + ";" + m.getName() + ";" + Arrays.toString(m.getCategories()) + ";" + m.getRating() + ";");
+        }
         launch(args);
-        System.out.println();
+
     }
 }
