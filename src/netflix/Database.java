c191b8c1578706493a043ce02013f26654594e65
julianbrandt <julianbrandt@godmail.dk>
2018-12-06 15:04:40 +0100

    Removed unnecessary imports

--- src/netflix/Database.java
+++ src/netflix/Database.java
@@ -6,14 +6,10 @@ import netflix.models.Saveable;
 import netflix.models.User;
 import netflix.models.UserType;
 import netflix.models.media.*;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import javax.xml.crypto.Data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
