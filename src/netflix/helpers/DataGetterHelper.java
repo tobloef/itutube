e71936231b713862bfc4963f11859b7ce3e67482
koshaan <koshaaniscool@gmail.com>
2018-12-07 13:36:09 +0100

    Update DataGetterHelper.java
    fixed seach method - unit tests coming soon

--- src/netflix/helpers/DataGetterHelper.java
+++ src/netflix/helpers/DataGetterHelper.java
@@ -8,12 +8,10 @@ public class DataGetterHelper{
 
     public  Media searchMedia(String name) {
         for (Media m : Database.getMediaList())
-            if (m.getName().equals(name)) {
+            if (m.getName().equals(name)) { //checker først hvis navnet er equal for film som ET
                 return m;
-            }
+            } else if (m.getName().startsWith(name)){ //ellers checker hvis det starter med, så man kan skrive "the god" og få the godfather
+                return m;}
         return null;
     }
 }
-
-
-
