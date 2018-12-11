dedf5901a608d3c60b0c9d9e6c7c45376ba69d7d
julianbrandt <julianbrandt@godmail.dk>
2018-12-12 00:45:07 +0100

    Update SearchResultContent.java

--- src/netflix/views/content/SearchResultContent.java
+++ src/netflix/views/content/SearchResultContent.java
@@ -1,10 +1,15 @@
 package netflix.views.content;
 
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
+import netflix.models.ImageButtonInfo;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class SearchResultContent extends VBox {
 
-    public SearchResultContent() {
+    public SearchResultContent(List<ImageButtonInfo> imageButtonInfoList) {
 
     }
 }
